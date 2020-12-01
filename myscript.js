c8108861bc65d2b7e134abe984c0891dc4a35dbd
rasyad <mrcrasyad26@gmail.com>
2020-12-01 08:09:45 +0700

    latihan ke-2

--- myscript.js
+++ myscript.js
@@ -1,17 +1,12 @@
 
 function menu(a) {
+	
 	var m = document.getElementsByClassName("submenu");
 	var l = m.length;
-
-	for (var i = 0; i < l; i++) {
+	for(var i = 0; i < l; i++){
 		m[i].style.display="none";
 	}
-
-	m.style.display="block";
-
+	
+	var sm=document.getElementById("sm"+a);
+	sm.style.display="block";
 }
-
-// function clmenu(a) {
-// 	var sm=document.getElementById("sm"+a);
-// 	sm.style.display="none";
-// }
